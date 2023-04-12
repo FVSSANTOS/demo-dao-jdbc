4f329632880f486db80e6a7722eda0f43e535e75
FVSSANTOS <flaviovss@yahoo.com>
2023-04-12 15:13:10 -0300

    Department entity class

---
+++ src/application/App.java
@@ -0,0 +1,10 @@
+package application;
+
+import model.enitities.Department;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        Department obj = new Department();
+    }
+}
