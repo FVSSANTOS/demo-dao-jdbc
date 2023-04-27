01bb36eff29031bfe3d504fcb86962b1797437fe
FVSSANTOS <flaviovss@yahoo.com>
2023-04-27 15:57:38 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -15,6 +15,10 @@ public class Program {
         Seller seller = new Seller(21,"Bob","bob@gmail.com",new Date(), 3000.0, obj);
 
         SellerDao sellerDao = new DaoFactory().createSellerDao();
-        System.out.println(seller);
+
+        Seller seller2 = sellerDao.findById(3);
+
+        System.out.println(seller2);
+        
     }
 }
