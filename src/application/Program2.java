836151c75c4b29ec88e471b9de059919e5ca9659
FVSSANTOS <flaviovss@yahoo.com>
2023-05-02 16:06:06 -0300

    update implamentation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -13,5 +13,10 @@ public class Program2 {
         Department department = new Department(null, "Food");
         departmentDao.insert(department);
         System.out.println("Inserted! New id = "+department.getId());
+
+        System.out.println("==== Test 2 department update ====");
+        department.setName("Test");
+        departmentDao.update(department);
+        System.out.println("Update completed!");
     }
 }
