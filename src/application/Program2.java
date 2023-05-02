06ae5b16d4a43fa3180b724811c85fad57627fc9
FVSSANTOS <flaviovss@yahoo.com>
2023-05-02 15:47:42 -0300

    Created DepartmentDaoJDBC and insert implementation

---
+++ src/application/Program2.java
@@ -0,0 +1,17 @@
+package application;
+
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.enitities.Department;
+
+public class Program2 {
+    public static void main(String [] args){
+
+        DepartmentDao departmentDao = new DaoFactory().createDepartmentDao();
+        
+        System.out.println("==== Test 1 department insert ====");
+        Department department = new Department(null, "Food");
+        departmentDao.insert(department);
+        System.out.println("Inserted! New id = "+department.getId());
+    }
+}
